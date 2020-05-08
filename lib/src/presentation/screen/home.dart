8e703f1877e0b3ea42b63b7daedbcabe522aca33
moreira <contato@eduardoem.com.br>
2020-05-08 09:47:12 -0300

    Remove default structure from flutter

---
+++ lib/src/presentation/screen/home.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/widgets.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Eai');
+  }
+}
