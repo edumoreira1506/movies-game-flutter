7450773a25703c0ff5c03468fbe7a190914f0462
moreira <contato@eduardoem.com.br>
2020-05-08 10:14:08 -0300

    Add categories request

---
+++ lib/src/model/dto/category.dart
@@ -0,0 +1,6 @@
+class CategoryDTO {
+  final String name;
+  final int id;
+
+  CategoryDTO(this.name, this.id);
+}
--- lib/src/presentation/screen/home.dart
+++ lib/src/presentation/screen/home.dart
@@ -1,5 +1,7 @@
 
 import 'package:flutter/widgets.dart';
+import 'package:movies_game/src/model/dto/category.dart';
+import 'package:movies_game/src/model/service/category_service.dart';
 import 'package:movies_game/src/presentation/widget/spinner.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,15 +10,29 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  bool _isLoading;
+  List<CategoryDTO> _categories = List();
+  CategoryService _service = CategoryService();
+
+  bool _isLoading = true;
 
   @override
   void initState() {
     super.initState();
+
+    _fetchCategories();
+  }
+
+  void _fetchCategories() async {
+    List<CategoryDTO> categories = await _service.all();
+
+    setState(() {
+      _categories = categories;
+      _isLoading = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Spinner();
+    return _isLoading ? Spinner() : Text('Carregou!');
   }
 }
